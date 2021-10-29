5c6ebce514fc4258c549ada776fc00ece6ac4e21
Tony_Yen <Tony_Yen@asus.com>
2021-10-29 15:40:03 +0800

    1539

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3b2d36164eb2138c83ae91d4fbffa4fc",
-"/": "3b2d36164eb2138c83ae91d4fbffa4fc",
+"index.html": "d18f606f176e47692aa2ed6e49ed2463",
+"/": "d18f606f176e47692aa2ed6e49ed2463",
 "main.dart.js": "ccc26b9ddc679dbf553539adbce94351",
 "manifest.json": "52571732fd4cf05ed748b11273077cd9",
 "version.json": "5d8d34e82231924748f3d9749c4c77da"
